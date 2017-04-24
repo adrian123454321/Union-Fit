68d7f5c855ceb5b169dffddebfd0b76917e1b2d2
adrian123454321 <adrian.lora12345@gmail.com>
2017-04-24 09:37:33 -0600

    Django edit

--- Union fit-django/usuario/models.py
+++ Union fit-django/usuario/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+class Usuario(object):
+	nombre= models.Charfield(max_length=20)
+	correo= models.Charfield(max_length=30)
+	edad=models.Charfield(max_length=2)
+	telefono=models.Charfield(max_length=20)
+	contraseña=models.Charfield(max_length=20)
+	usuario=models.Charfield(max_length=15)
 
+	def__str__(self):
+		self.nombre
+		self.correo
+		self.edad
+		self.telefono
+		self.contraseña
+		self.usuario
 # Create your models here.
