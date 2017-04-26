7c7baee640190946a4fb837a00703fe460c88540
adrian123454321 <adrian.lora12345@gmail.com>
2017-04-26 09:29:57 -0600

    Models

--- gym/Gym/asociar/models.py
+++ gym/Gym/asociar/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+ class Asociar(models.Model):
+ 	nombre= models.Charfield(max_length=50)
+ 	correo= models.Charfield(max_length=50)
+ 	direccion=models.Charfield(max_length=50)
+ 	telefono=models.Charfield(max_length=20)
+ 	precio=models.Charfield(max_length=20)
+ 
+ 	def__str__(self):
+ 
+ 		self.nombre
+ 		self.correo
+ 		self.direccion
+ 		self.telefono
+ 		self.precio
+from django.db import models
 
 # Create your models here.
