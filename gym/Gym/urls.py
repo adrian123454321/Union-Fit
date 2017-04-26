e32d24f5393fb8469b3b2dbc371a07bf4de51db5
adrian123454321 <adrian.lora12345@gmail.com>
2017-04-26 09:25:15 -0600

    Django settings

--- gym/Gym/urls.py
+++ gym/Gym/urls.py
@@ -1,4 +1,4 @@
-"""gym URL Configuration
+"""Gym URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- gym/Gym/wsgi.py
+++ gym/Gym/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gym project.
+WSGI config for Gym project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gym.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gym.settings")
 
 application = get_wsgi_application()
